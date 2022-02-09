e7dc7e264354e580c6d6427bb3fe9a585ea331e2
Lucky <641787296@qq.com>
2022-02-09 08:18:37 +0800

    art: 升级boot到2.2.6.RELEASE 和 easyexcel 3.0.5

--- easyexcel-demo/src/main/java/com/zwj/StartApplication.java
+++ easyexcel-demo/src/main/java/com/zwj/StartApplication.java
@@ -4,7 +4,10 @@ package com.zwj;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,20 +18,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 与WebApplication相同
  * @SpringBootApplication 注解的程序入口类已经包含@Configuration，不含@ServletComponentScan
  * @author admin
- *
+ * SpringBootApplication(scanBasePackages = {"com.zwj.*"}, exclude = {RedisAutoConfiguration.class})
  */
 @SpringBootApplication
 @EnableAsync
 public class StartApplication {
 	private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
-
-
 	public static void main(String[] args) {
 		log.info("启动服务中...");
 		ConfigurableApplicationContext context = SpringApplication.run(StartApplication.class, args);
 		log.info("服务已启动...");
 	}
-
-
-
 }
